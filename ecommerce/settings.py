251b2083d9cd589a7288406452c37cff32660dce
clems-dev-maker <clementcathala430@gmail.com>
2024-11-01 10:03:04 +0100

    Protection mot de passe SMTP

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -13,12 +13,13 @@ import os
 from pathlib import Path
 import environ
 
-# Initialise les variables d'environnement
-env = environ.Env()
-environ.Env.read_env()  # Lit les variables d'un fichier .env
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Initialise les variables d'environnement
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -49,10 +50,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = env('EMAIL_HOST_USER')   # Remplace par ton adresse Gmail
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')   # Le mot de passe d'application généré précédemment
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')  # Remplace par ton adresse Gmail
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')  # Le mot de passe d'application généré précédemment
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER  # Email d'envoi par défaut
-
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
