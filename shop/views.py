d3738b52e3337cfc4ec3f4b20c7eb55bd54753a8
clems-dev-maker <clementcathala430@gmail.com>
2026-04-22 09:30:13 +0200

    Correction du template cart_detail.html(quantité en double

--- shop/views.py
+++ shop/views.py
@@ -37,8 +37,9 @@ def add_to_cart(request, product_id):
     cart, _ = Cart.objects.get_or_create(user=request.user)
     cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product)
 
-    cart_item.quantity += 1
-    cart_item.save()
+    if not created:
+        cart_item.quantity += 1
+        cart_item.save()
 
     # Calcul du nouveau compteur
     cart_count = sum(item.quantity for item in cart.cartitem_set.all())
